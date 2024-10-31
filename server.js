57f4156dba7664e51d65784381af7b913487908e
lukwagoasuman <lukwagoasuman236@gmail.com>
2024-10-31 17:50:50 +0300

    Initial commit

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection'); // Assuming `connection.js` exports a function
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const passport = require('passport');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -16,10 +15,7 @@ const app = express();
 // FCC testing purposes
 fccTesting(app);
 
-// Serve static files
 app.use('/public', express.static(process.cwd() + '/public'));
-
-// Middleware for parsing JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -53,54 +49,6 @@ myDB(async (client) => {
   app.route('/').get((req, res) => {
     res.render('index', { title: 'Database Error', message: 'Unable to connect to database' });
   });
-});
-
-let New = new LocalStrategy((username, password, done) => {
-  console.log(`Attempting login for username: ${username}`);
-  
-  myDB.collection('users').findOne({ username: username }, (err, user) => {
-    if (err) {
-      console.error(`Error accessing DB: ${err}`);
-      return done(err);
-    }
-    if (!user) {
-      console.warn(`User not found: ${username}`);
-      return done(null, false, { message: 'Incorrect username' });
-    }
-    
-    // Check if the passwords match
-    if (!bcrypt.compareSync(password, user.password)) {
-      console.warn(`Password mismatch for user: ${username}`);
-      return done(null, false, { message: 'Incorrect password' });
-    }
-    
-    console.log(`User ${username} authenticated successfully`);
-    return done(null, user);
-  });
-});
-
-passport.serializeUser((user, done) => {
-  console.log(`Serializing user ID: ${user._id}`);
-  done(null, user._id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  console.log(`Deserializing user ID: ${id}`);
-  try {
-    const user = await myDB.collection('users').findOne({ _id: id });
-    if (!user) {
-      console.warn(`User not found during deserialization: ${id}`);
-      return done(null, doc);
-    }
-    console.log(`User deserialized successfully: ${id}`);
-    done(null, user);
-  } catch (err) {
-    console.error(`Error during deserialization: ${err}`);
-    done(err);
-  }
-
-  passport.use(New)
-
 });
 
 const PORT = process.env.PORT || 8080;
