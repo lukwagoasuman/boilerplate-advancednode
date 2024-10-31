9b4be53cbaba3ae3c2ceb98c4121dede1b248782
lukwagoasuman <lukwagoasuman236@gmail.com>
2024-10-31 17:51:13 +0300

    initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection'); // Assuming `connection.js` exports a function
+const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const passport = require('passport');
 const session = require('express-session');
@@ -23,11 +23,34 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure: false } // Set to true in production with HTTPS
+  cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Configure Local Strategy for Passport
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: 'Incorrect username' });
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false, { message: 'Incorrect password' });
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: id }, (err, user) => {
+    if (err) return done(err);
+    done(null, user);
+  });
+});
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -36,16 +59,29 @@ app.set('views', './views/pug');
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
 
-
-
-  // Root route to render the 'index.pug' view
   app.route('/').get((req, res) => {
-    res.render('index', { title: 'Connected to Database', message: 'Please log in' });
+    res.render('index', { title: 'Connected to Database', message: 'Please log in', showLogin: true });
+  });
+
+  app.route('/login')
+    .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+      console.log(`User ${req.body.username} attempted to log in.`);
+      res.redirect('/profile');
+    });
+
+  function ensureAuthenticated(req, res, next) {  //This is Application Security DAY 33 
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile');
   });
 
 }).catch(e => {
-  console.error('Database connection failed:', e); // Log the error
-  // Error route if the database connection fails
+  console.error('Database connection failed:', e);
   app.route('/').get((req, res) => {
     res.render('index', { title: 'Database Error', message: 'Unable to connect to database' });
   });
