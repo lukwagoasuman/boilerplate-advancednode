1f0659ea55f2ac499d70ca5dab54f407b7dce7bc
lukwagoasuman <lukwagoasuman236@gmail.com>
2024-11-05 11:30:13 +0300

    Initial commit

--- server.js
+++ server.js
@@ -79,6 +79,19 @@ myDB(async (client) => {
     res.render('profile', { username: req.user.username });
   });
 
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+app.use((req, res, next) => {
+  res.status(404)
+  .type('text')
+  .send('Not Found');
+
+});
+
 }).catch(e => {
   console.error('Database connection failed:', e);
   app.get('/', (req, res) => {
